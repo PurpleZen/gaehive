db1b46be5cf21c66bc9be073b1c6af38cb561ee2
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-11 23:58:37 +0000

    Update

--- api/index.js
+++ api/index.js
@@ -2,7 +2,17 @@ const app = require('express')();
 const jwt = require('jsonwebtoken');
 import { createClient } from '@supabase/supabase-js'
 export const supabase = createClient('https://krtafulrwvxmpzmuqjbf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4')
-const Cookies = require('js-cookie');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+const cookieOptions = {
+  path: '/',                    // Send the cookie to all routes
+  httpOnly: true,               // Make the cookie not accessible by document.cookie
+  sameSite: 'lax',              // Allow incoming links (but not requests) to include the cookie
+  secure: true,                 // Make sure the cookie is secure
+  maxAge: 7 * 24 * 3600 * 1000  // The cookie expires in 7 days
+};
 
 app.get('/api', async (req, res) => {
   const result = await fetch('https://auth.itinerary.eu.org/api/auth/verifyToken?privateCode=' + req.query.privateCode);
@@ -10,8 +20,8 @@ app.get('/api', async (req, res) => {
 
   if (json.valid) {
     const token = jwt.sign({ name: json.username, role: "authenticated", level: "manager" }, process.env['SUPABASE_JWT'], { expiresIn: '14 days' });
-    res.status(200).setHeader('Set-Cookie', serialize('mytoken', token, { path: "/" }));
-    const { user, error } = supabase.auth.setAuth(Cookies.get("mytoken"))
+    res.cookie('mytoken', token, cookieOptions);
+    const { user, error } = supabase.auth.setAuth(req.cookies.mytoken)
     
   } else {
     return res.json({ token: "invalid" })
