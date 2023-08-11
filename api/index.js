ed943cc8c8e7a6c62d955817f76dba30e97f1bef
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-11 23:52:42 +0000

    Update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const jwt = require('jsonwebtoken');
 import { createClient } from '@supabase/supabase-js'
-const supabase = createClient('https://krtafulrwvxmpzmuqjbf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4')
+export const supabase = createClient('https://krtafulrwvxmpzmuqjbf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4')
 const Cookies = require('js-cookie');
 
 app.get('/api', async (req, res) => {
@@ -11,7 +11,7 @@ app.get('/api', async (req, res) => {
   if (json.valid) {
     const token = jwt.sign({ name: json.username, role: "authenticated", level: "manager" }, process.env['SUPABASE_JWT'], { expiresIn: '14 days' });
     res.status(200).setHeader('Set-Cookie', serialize('mytoken', token, { path: "/" }));
-    supabase.auth.setAuth(Cookies.get("mytoken"))
+    const { user, error } = supabase.auth.setAuth(Cookies.get("mytoken"))
     
   } else {
     return res.json({ token: "invalid" })
