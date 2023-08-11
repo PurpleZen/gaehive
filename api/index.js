a1d3a88a291c5278534cc46025bb55a353934eb8
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-11 23:44:49 +0000

    Update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const jwt = require('jsonwebtoken');
-import { supabase } from './src/lib/supabaseClient'
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://krtafulrwvxmpzmuqjbf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4')
 const Cookies = require('js-cookie');
 
 app.get('/api', async (req, res) => {
