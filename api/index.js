21109286608ee11c0799240ff31c2918912978f3
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-12 01:02:35 +0000

    test

--- api/index.js
+++ api/index.js
@@ -1,19 +1,12 @@
 const app = require('express')();
 const jwt = require('jsonwebtoken');
-import { createClient } from '@supabase/supabase-js'
-const options = {
-  auth: {
-    persistSession: false
-}
-}
-export const supabase = createClient('https://krtafulrwvxmpzmuqjbf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4', options)
 const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 
 const cookieOptions = {
   path: '/',                    // Send the cookie to all routes
-  httpOnly: true,               // Make the cookie not accessible by document.cookie
+  httpOnly: false,               // Make the cookie not accessible by document.cookie
   sameSite: 'lax',              // Allow incoming links (but not requests) to include the cookie
   secure: true,                 // Make sure the cookie is secure
   maxAge: 7 * 24 * 3600 * 1000  // The cookie expires in 7 days
@@ -26,8 +19,7 @@ app.get('/api', async (req, res) => {
   if (json.valid) {
     const token = jwt.sign({ name: json.username, role: "authenticated", level: "manager" }, process.env['SUPABASE_JWT'], { expiresIn: '14 days' });
     res.cookie('mytoken', token, cookieOptions);
-    const { user, error } = supabase.auth.setSession(req.cookies.mytoken)
-    
+    res.redirect("/")
   } else {
     return res.json({ token: "invalid" })
   }
