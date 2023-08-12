bfe8f279ecb2aeec70f54fc0fc218d3fcc01c47f
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-12 01:39:16 +0000

    update

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,18 @@
   import { createClient } from '@supabase/supabase-js'
 
-  export const supabase = createClient('https://krtafulrwvxmpzmuqjbf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4',{global: { headers: {
-  Authorization: `Bearer ${document.cookie.slice(8)}`
-}}})
+  export const supabase = createClient(
+    'https://krtafulrwvxmpzmuqjbf.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4',
+    {
+      auth: {
+        autoRefreshToken: false,
+        detectSessionInUrl: false,
+        persistSession: true
+      },
+      global: { 
+        headers: {
+          Authorization: `Bearer ${document.cookie.slice(8)}`
+        }
+      }
+    }
+  )
