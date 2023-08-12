fca6c392f32dd3a16380fab35390e2b1bf8e0a3b
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-12 00:23:13 +0000

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const app = require('express')();
 const jwt = require('jsonwebtoken');
 import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient('https://krtafulrwvxmpzmuqjbf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4')
+const options = {
+  auth: {
+    persistSession: false
+}
+export const supabase = createClient('https://krtafulrwvxmpzmuqjbf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4', options)
 const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
