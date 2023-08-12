83758cbc55760b7dfa8df089c18bbe985750c826
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-12 00:30:28 +0000

    fix

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const options = {
   auth: {
     persistSession: false
 }
+}
 export const supabase = createClient('https://krtafulrwvxmpzmuqjbf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4', options)
 const cookieParser = require('cookie-parser');
 
