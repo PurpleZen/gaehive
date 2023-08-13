590e04a562cab9d8f343d3b0af837e9f304b0536
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-13 21:30:16 +0000

    fix?

--- src/pages/managers/Managers.vue
+++ src/pages/managers/Managers.vue
@@ -4,7 +4,7 @@
       <div class="queue">
         <div v-if="loading" class="loader"></div>
         <h1 v-if=!loading class="greeting">Our Managers</h1>
-        <span v-if=!loading>The Gaehive Scratch studio has around <b style="font-family:serif">{{ this.active }}</b> active managers that help keep the studio a safe and welcoming place for all. Managers rotate the role of Studio Host daily, giving them all opportunities to edit the studio and have some fun!<br>Here you can see the host queue and meet our wonderful managers!<b v-if="username && manager == 'true' || admin == 'true'"><br>Managers: You can click the star button on users to make them show as the current host!</b></span><button @click='newCountry()'>hi</button>
+        <span v-if=!loading>The Gaehive Scratch studio has around <b style="font-family:serif"></b> active managers that help keep the studio a safe and welcoming place for all. Managers rotate the role of Studio Host daily, giving them all opportunities to edit the studio and have some fun!<br>Here you can see the host queue and meet our wonderful managers!<b v-if="username && manager == 'true' || admin == 'true'"><br>Managers: You can click the star button on users to make them show as the current host!</b></span><button @click='newCountry()'>hi</button>
         
         <router-link v-if="username && manager == 'true' || admin == 'true'" to="/managers/edit" class="button">Edit Managers</router-link>
         <div v-if=!loading class="break"></div>
@@ -25,25 +25,6 @@
     </div>
   </div>
 </template>
-
-<script>
-  export default {
-    data() {
-      return {
-        loading: null,
-        request: null,
-        error: null,
-        hosts: null,
-        nexthost: null,
-        list: null,
-        active: null,
-        username: null,
-        admin: null,
-        manager: null
-      }
-    },
-  }
-</script>
 
 <script setup>
   import { ref, onMounted } from 'vue'
