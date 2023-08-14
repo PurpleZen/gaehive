54a762c219431bb9450bc5136ce68fd2b5312d11
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-14 22:23:36 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
     <div v-if="loading" class="loader">Loading...</div>
     <div v-if="error" class="error"><div class="material-symbols-rounded">error</div><span>Uh oh! Looks like something went wrong.</span><div @click="error = !error" class="button">Ok</div></div>
     <div v-if="!loading && !error" class="loader-placehold"></div>
-    <img width="200px" v-if="username" :src="'https://uploads.scratch.mit.edu/users/avatars/' + id + '.png'">
+    <div class="hello"
+    <img v-if="username" :src="'https://uploads.scratch.mit.edu/users/avatars/' + id + '.png'">
     <h2 @click.right="this.secret = true" v-if="username">Hello{{ mellie }} {{ username }}</h2>
     <h2 @click.right="this.secret = true" v-if="!username">Hello</h2>
+    </div>
     
     <a class='button' href="https://scratch.mit.edu/studios/5842709/comments">scratch studio</a>
     <router-link class='button' to="/">home</router-link>
@@ -233,6 +235,18 @@ textarea, .preview {
   color: var(--acc2);
   border-left: solid 3.5px var(--bg);
   padding-left: 5px;
+}
+
+.hello {
+  display: flex;
+  align-items: center;
+}
+
+.hello img {
+  width: 50px;
+  border-radius: 100%;
+  border: var(--bg) solid 3px;
+  height: 50px;
 }
 
 .sidelinks {
--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,8 @@
   import { createClient } from '@supabase/supabase-js'
-  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-  const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
   export const supabase = createClient(
-    supabaseUrl,
-    supabaseAnonKey,
+    'https://krtafulrwvxmpzmuqjbf.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydGFmdWxyd3Z4bXB6bXVxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3ODcxOTMsImV4cCI6MjAwNzM2MzE5M30.febzjg767tHBjXEcPF1Z1SuAG0k30L4JjEoSuIgihI4',
     {
       auth: {
         autoRefreshToken: false,
