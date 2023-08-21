8b880aa46faa0989552a949e789b9998cab6b6cb
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-21 00:11:43 +0000

    fix..?

--- api/login/index.js
+++ api/login/index.js
@@ -17,7 +17,7 @@ import { createClient } from '@supabase/supabase-js'
   const supabaseUrl = process.env.VITE_SUPABASE_URL
   const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY
 
-  export const supabase = createClient(
+  const supabase = createClient(
     supabaseUrl,
     supabaseAnonKey,
     {
