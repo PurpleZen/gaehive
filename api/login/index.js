9e734f088a6d66c00d4c5835386b58633c32aa60
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-21 00:41:24 +0000

    test

--- api/login/index.js
+++ api/login/index.js
@@ -39,15 +39,20 @@ app.get('/api/login', async (req, res) => {
 
   var level = "user"
   const { data } = await supabase.from('managers').select('data')
-  const managers = data[0].data
-  if (managers.includes(json.username) || json.username == "LegoManiac04") {
+  const managers = await data[0].data
+  for (var i = 0; i < managers.length; i++) {
+    if (managers[i].name == json.username) {
+        level = "manager"
+      }
+    }
+  if (json.username == "LegoManiac04") {
     level = "manager"
   }
 
   if (json.valid) {
     const token = jwt.sign({ name: json.username, role: "authenticated", level: "manager" }, process.env['SUPABASE_JWT'], { expiresIn: '14 days' });
     res.cookie('mytoken', token, cookieOptions);
-    return res.json({ managers: managers })
+    res.redirect("/?user=" + btoa(JSON.stringify({'username': json.username, 'id': id, 'level': level})))
   } else {
     return res.json({ token: "invalid" })
   }
