cd9ce622a4c5d25eb3674f1692735547b94c0762
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-08-26 23:21:31 +0000

    test..?

--- api/user/index.js
+++ api/user/index.js
@@ -14,8 +14,7 @@ const apiRequestLimiter = rateLimit({
 app.use(apiRequestLimiter)
 
 // New posts are fetched from the studio here, compiled, and sent back to the backend server to be stored!
-app.get('/api/user', (req, res) => {
-  var user = req.query.user
+app.get('/api/hivezine', (req, res) => {
   fetch(
     `https://api.scratch.mit.edu/studios/33586934/comments?limit=1`,
     {
@@ -25,7 +24,34 @@ app.get('/api/user', (req, res) => {
     }
   ).then((response)=>{return response.json();}).then(data=>{
 
-    return res.json(data)
+    const title = data[0].content;
+    const user = data[0].author.username;
+    const uid = data[0].author.id;
+    const id = data[0].id;
+
+    fetch(
+    `https://api.scratch.mit.edu/studios/33586934/comments/` + id + `/replies/?limit=25`,
+    {
+      headers: {
+        "User-Agent": "Mozilla/5.0 Gaehive",
+      },
+    }
+  ).then((response)=>{return response.json();}).then(data=>{
+
+    var post = data[0].content
+    for ( var i = 1; i < data.length; i++ ) {
+      post = post + " " + data[i].content;
+    }
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+      const d = new Date();
+      let date = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear()
+    
+    res.json([{
+      user: user, uid: uid, date: date, title: title, post: post, pid: id
+    }])
+  })
   })
 });
 
