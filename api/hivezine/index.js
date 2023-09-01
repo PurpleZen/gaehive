8217e2d686d7fc119119aa6009abf3c05d5136c4
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2023-09-01 01:02:17 +0000

    test

--- api/hivezine/index.js
+++ api/hivezine/index.js
@@ -24,34 +24,8 @@ app.get('/api/hivezine', (req, res) => {
     }
   ).then((response)=>{return response.json();}).then(data=>{
 
-    const title = data[0].content;
-    const user = data[0].author.username;
-    const uid = data[0].author.id;
-    const id = data[0].id;
-
-    fetch(
-    `https://api.scratch.mit.edu/studios/33586934/comments/` + id + `/replies/?limit=25`,
-    {
-      headers: {
-        "User-Agent": "Mozilla/5.0 Gaehive",
-      },
-    }
-  ).then((response)=>{return response.json();}).then(data=>{
-
-    var post = data[0].content
-    for ( var i = 1; i < data.length; i++ ) {
-      post = post + " " + data[i].content;
-    }
-
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-      const d = new Date();
-      let date = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear()
     
-    res.json([{
-      user: user, uid: uid, date: date, title: title, post: post, pid: id
-    }])
-  })
+    res.json(data)
   })
 });
 
