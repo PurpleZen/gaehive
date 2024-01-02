97a6f6a138c2338401faccb9a989808298db2c8c
PurpleZen <87787046+PurpleZen@users.noreply.github.com>
2024-01-02 22:57:22 +0000

    test

--- api/birthdays/index.js
+++ api/birthdays/index.js
@@ -1,9 +1,8 @@
 const app = require('express')();
 const jwt = require('jsonwebtoken');
-const rateLimit = require('express-rate-limit');
 const fetch = require('cross-fetch');
 
-const token = jwt.sign({ role: "authenticated", from: "server" }, process.env['SUPABASE_JWT'], { expiresIn: '1m' });
+const token = jwt.sign({ role: "authenticated", cron: process.env['CRON_SECRET'] }, process.env['SUPABASE_JWT'], { expiresIn: '1m' });
 
 import { createClient } from '@supabase/supabase-js'
 
@@ -27,35 +26,31 @@ const supabase = createClient(
   }
 )
 
-// Limit how many requests can be made to prevent users from potentially spamming the Scratch servers.
-const apiRequestLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 2,
-  message: { 'error': 'too many requests' },
-  standardHeaders: true, 
-  legacyHeaders: false,
-})
-
-app.use(apiRequestLimiter)
-
 // New posts are fetched from the studio here, compiled, and sent back to the backend server to be stored!
 app.get('/api/birthdays', (req, res) => {
-  fetch(
-    `https://api.scratch.mit.edu/studios/34421126`,
-    {
-      headers: {
-        "User-Agent": "Mozilla/5.0 Gaehive",
-      },
-    }
-  ).then((response)=>{return response.json();}).then(async data=>{
+  const authHeader = req.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  } else {
+    fetch(
+      `https://api.scratch.mit.edu/studios/34421126`,
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0 Gaehive",
+        },
+      }
+    ).then((response)=>{return response.json();}).then(async data=>{
 
-    const { error } = await supabase
-      .from('birthdays')
-      .update({ users: JSON.parse(data.description) })
-      .eq("id", 1)
+      const { error } = await supabase
+        .from('birthdays')
+        .update({ users: JSON.parse(data.description) })
+        .eq("id", 1)
 
-    res.json([{ birthdays: "updated", users: JSON.parse(data.description), errors: error }])
-  })
+      res.json([{ birthdays: "updated 🎉" }])
+    })
+  }
 });
 
 module.exports = app;
