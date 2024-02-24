78b1dd05e2ddd2b990c6be2e68fa9833cac45ad8
LegoManiac04 <87787046+PurpleZen@users.noreply.github.com>
2024-02-24 23:54:36 +0000

    Updates & Fixes

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,24 @@
 <div class="sidebar">
 
   <div class="hello">
-    <img src="/favicon.ico">
-    <h2>Hello :)</h2>
+    <img v-if="username" :src="'https://uploads.scratch.mit.edu/get_image/user/' + id + '_500x500.png'">
+    <img v-if="!username" src="/favicon.ico">
+    <h5 v-if="username">Hello{{ mellie }},<br>{{ username }}</h5>
+    <h5 v-if="!username">Hello! 🐝</h5>
+    <sub class="status">{{ this.status }}</sub>
   </div>
 
-  <router-link class='sidebuttonactive' to="/">About</router-link>
+  <div class="sidelinks">
+    <router-link class='sidebuttonactive' to="/">About</router-link>
 
-  <a class='sidebutton' href="https://scratch.mit.edu/studios/5842709/comments" target="_blank">Scratch Studio</a>
+    <a class='sidebutton' href="https://scratch.mit.edu/studios/5842709/comments" target="_blank">Scratch Studio</a>
 
-  <a class='sidebutton' href="https://gaehive-legacy.vercel.app/" target="_blank">Legacy Website</a>
+    <a class='sidebutton' href="https://gaehive-legacy.vercel.app/" target="_blank">Legacy Website</a>
+  </div>
 
+  <div class="loginSettingsBox">
+    
+  </div>
   
 </div>
   <div class="page">
@@ -38,18 +46,16 @@
 
   :root {
     --bg: #f6b93c;
-    --sb: #fee5a1;
+    --sb: #ffe098;
     --acc: #e58c3f;
     --acc2: #483248;
-    --acclt: #ee824b66;
+    --acclt: #d18d6a66;
     --brk: #0006;
-    --txt: #444;
+    --txt: #473241;
     --txtstr: #000;
     --btxt: #fee5a1;
-    --slnk: #676700;
-    --slnkh: #484800;
-    --plnk: #573c57;
-    --plnkh: #392839;
+    --plnk: #732e6e;
+    --plnkh: #4c1e47;
     --imp: #d22727;
   }
 
@@ -137,76 +143,6 @@
     height: 35px;
   }
 
-  .sidebar {
-    background-color: var(--sb);
-    width: 20%;
-    padding: 30px;
-    flex-shrink: 0;
-    display: grid;
-    align-content: center;
-    transition: all ease 0.2s;
-    z-index: 2;
-  }
-
-  .sidebar h2 {
-    font-family: 'Agbalumo';
-    font-size: larger;
-    line-height: 22px;
-    color: var(--acc2);
-    margin: 0 0 0 8px;
-    text-shadow: var(--acclt) 1.5px 1.5px;
-  }
-
-  .hello {
-    display: flex;
-    align-items: center;
-    margin: 0 0 10px 5px;
-  }
-
-  .hello img {
-    width: 50px;
-    height: 50px;
-    border-radius: 100%;
-  }
-
-  .sidebutton {
-    display: inline-block;
-    background-color: transparent;
-    color: var(--txt);
-    text-decoration: none;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-family: inherit;
-    font-size: inherit;
-    text-align: left;
-    padding: 5px 15px;
-    margin-bottom: 2px;
-    transition: padding linear 0.05s;
-  }
-
-  .sidebuttonactive {
-    display: inline-block;
-    background-color: var(--acc);
-    color: var(--btxt);
-    text-decoration: none;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-family: inherit;
-    font-size: inherit;
-    text-align: left;
-    padding: 5px 15px;
-    padding-left: 20px;
-    margin-bottom: 2px;
-  }
-
-  .sidebutton:hover {
-    padding-left: 20px;
-    background-color: var(--acclt);
-    color: var(--txtstr);
-  }
-
   .button, button {
     width: fit-content;
     display: inline-flex;
@@ -221,16 +157,166 @@
     text-align: left;
     padding: 5px 15px;
     margin: 5px 3px;
-    border-radius: 5px;
+    border-radius: 10px;
   }
 
-  button .material-symbols-rounded {
+  .sidebar {
+    background-color: var(--sb);
+    padding: 50px;
+    border-radius: 0 20px 20px 0;
+    display: grid;
+    justify-content: center;
+    align-content: space-around;
+    overflow: clip;
+    transition: all ease 0.1s;
+  }
+
+  .sidebar h2 {
+    font-family: 'Agbalumo';
+    font-size: larger;
+    line-height: 22px;
+    color: var(--acc2);
+    margin: 0 0 0 8px;
+    text-shadow: var(--acclt) 1.5px 1.5px;
+  }
+
+  .mobilemenu {
+    display: none;
+  }
+
+  .hello {
+    display: grid;
+    align-content: center;
+    justify-items: center;
+  }
+
+  .hello h5 {
+    margin: 0;
+    color: var(--acc2);
+    font-family: agbalumo;
+    text-align: center;
+    line-height: 20px;
+    font-size: 20px;
+  }
+
+  .hello img {
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    border: var(--sb) solid 6px;
+    outline: var(--acc) solid 3px;
+    margin: 10px;
+  }
+
+  .status {
+    margin-bottom: -15px;
+    color: var(--acc2);
+    font-weight: bold;
+  }
+
+  .sidelinks {
+    display: grid;
+  }
+
+  .sidebutton {
+    display: inline-block;
+    background-color: transparent;
+    color: var(--txt);
+    text-decoration: none;
+    border: none;
+    border-radius: 10px 0 0 10px;
+    cursor: pointer;
+    font-family: inherit;
+    font-size: 14px;
+    text-align: left;
+    padding: 5px 15px;
+    padding-right: 50px;
+    margin-right: -50px;
+    transition: all ease 0.05s;
+    white-space: nowrap;
+  }
+
+  .sidebuttonactive {
+    display: inline-block;
+    background-color: var(--bg);
+    color: var(--txt);
+    font-weight: bold;
+    text-decoration: none;
+    border: none;
+    border-radius: 10px 0 0 10px;
+    cursor: pointer;
+    font-family: inherit;
+    font-size: 14px;
+    text-align: left;
+    padding: 5px 15px;
+    padding-left: 20px;
+    margin-right: -50px;
+    position: relative;
+  }
+
+  .sidebutton:hover {
+    padding-left: 20px;
+    padding-right: 45px;
+    background-color: var(--acclt);
+    color: var(--txtstr);
+  }
+
+  .sidebuttonactive::before,
+  .sidebuttonactive::after {
+    content: "";
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    animation: blobin ease 0.1s;
+    right: -10px;
+  }
+  .sidebuttonactive::before {
+    bottom: 19.5px;
+    border-bottom: 10px solid var(--bg);
+    border-right: 10px solid var(--bg);
+    border-bottom-right-radius: 20px;
+  }
+  .sidebuttonactive::after {
+    top: 19.5px;
+    border-top: 10px solid var(--bg);
+    border-right: 10px solid var(--bg);
+    border-top-right-radius: 20px;
+  }
+
+  .loginSettingsBox{
+    display: flex;
+    justify-content: center;
+  }
+
+  .login {
+    display: flex;
+    background-color: var(--acc);
+    color: var(--btxt);
+    font-family: inherit;
+    font-size: 10px;
+    padding: 5px 11px;
+    margin: 2px;
+    align-items: center;
+    border-radius: 10px;
+    width: fit-content;
+    text-decoration: none;
+    white-space: nowrap;
+    cursor: pointer;
+    border: none;
+  }
+
+  .login .material-symbols-rounded {
+    width: 10px;
+    overflow: clip;
     font-size: inherit !important;
+    margin-left: 5px;
+    margin-bottom: -1.7px;
   }
 
-  .button:hover, .button:focus, button:hover, .clearbutton:hover, button:focus {
-    outline: var(--acclt) solid 2px;
+  .login:hover {
+    outline: var(--bg) solid 2px;
   }
+
 </style>
 
 <script>
